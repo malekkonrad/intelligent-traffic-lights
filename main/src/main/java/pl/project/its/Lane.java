1122097ee8b90318c8392ef65b5987dc4f442987
malekkonrad <konrad.malek1@gmail.com>
2025-04-08 22:30:46 +0200

    added Lane, LaneManager to split each direction into separate lanes

---
+++ main/src/main/java/pl/project/its/Lane.java
@@ -0,0 +1,31 @@
+package pl.project.its;
+
+import lombok.Getter;
+import lombok.Setter;
+import pl.project.its.directions.Direction;
+
+import java.util.*;
+
+@Getter
+public class Lane{
+
+    private final List<Direction> allowedDestinations;
+    @Setter
+    private Queue<Vehicle> vehicles;
+
+
+    public Lane(List<Direction> allowedExits) {
+        this.allowedDestinations = allowedExits;
+        this.vehicles = new LinkedList<>();
+    }
+
+
+    public boolean allows(Direction end) {
+        return allowedDestinations.contains(end);
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        vehicles.add(vehicle);
+    }
+
+}
