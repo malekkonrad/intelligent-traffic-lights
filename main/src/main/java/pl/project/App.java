956bb41f9ec239ae53cdc6beea95f6c3e905eda4
malekkonrad <konrad.malek1@gmail.com>
2025-04-08 18:50:26 +0200

    updated App, JsonReader, JsonWriter

--- main/src/main/java/pl/project/App.java
+++ main/src/main/java/pl/project/App.java
@@ -1,10 +1,19 @@
 package pl.project;
 
 
+import pl.project.its.Intersection;
+import pl.project.its.Vehicle;
 import pl.project.json.JsonReader;
 import pl.project.json.JsonWriter;
+import pl.project.json.structures.input.Command;
+import pl.project.json.structures.input.CommandList;
+import pl.project.json.structures.output.StepStatus;
+import pl.project.json.structures.output.StepStatusList;
 
 import java.io.IOException;
+import java.util.Arrays;
+
+import java.util.List;
 
 /**
  * Main program that runs intelligent traffic lights
@@ -13,8 +22,36 @@ public class App
 {
     public static void main( String[] args ) throws IOException {
         JsonReader jsonReader = new JsonReader("commands.json");
+
+        CommandList commandList = jsonReader.read();
+
+
+
+        // Obiekt w którym będę zapisywał statusy
+        StepStatusList stepStatusList = new StepStatusList();
+
+
+
+        Intersection intersection = new Intersection();
+
+        // Przejście po komendach
+        for (Command cmd : commandList.getCommands()) {
+            System.out.println("Type: " + cmd.getType());
+            if ("addVehicle".equals(cmd.getType())) {
+                intersection.addVehicle(new Vehicle(cmd.getVehicleId(),0, cmd.getStartRoad(), cmd.getEndRoad()));
+            }else{
+                List<String> leftVehicle = intersection.step();
+
+                // add step to list that will be saved into json
+                stepStatusList.addStep(leftVehicle);
+            }
+        }
+
+
+        // zapis
         JsonWriter jsonWriter = new JsonWriter("output.json");
-        jsonWriter.writeToFile();
+        jsonWriter.writeToFile(stepStatusList);
+
 
     }
 }
--- utils/src/main/java/pl/project/json/JsonReader.java
+++ utils/src/main/java/pl/project/json/JsonReader.java
@@ -9,22 +9,28 @@ import java.io.IOException;
 
 public class JsonReader {
 
+    CommandList commandList;
+
     public JsonReader(String filePath) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
 
         // Wczytanie JSON-a z pliku
-        CommandList commandList = mapper.readValue(new File(filePath), CommandList.class);
+        this.commandList = mapper.readValue(new File(filePath), CommandList.class);
 
         // Przejście po komendach
-        for (Command cmd : commandList.getCommands()) {
-            System.out.println("Type: " + cmd.getType());
-            if ("addVehicle".equals(cmd.getType())) {
-                System.out.println("  ID: " + cmd.getVehicleId());
-                System.out.println("  Start: " + cmd.getStartRoad());
-                System.out.println("  End: " + cmd.getEndRoad());
-            }
-        }
+//        for (Command cmd : commandList.getCommands()) {
+//            System.out.println("Type: " + cmd.getType());
+//            if ("addVehicle".equals(cmd.getType())) {
+//                System.out.println("  ID: " + cmd.getVehicleId());
+//                System.out.println("  Start: " + cmd.getStartRoad());
+//                System.out.println("  End: " + cmd.getEndRoad());
+//            }
+//        }
 
+    }
+
+    public CommandList read(){
+        return commandList;
     }
 
 
