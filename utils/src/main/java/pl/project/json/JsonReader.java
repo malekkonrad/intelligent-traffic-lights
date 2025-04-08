c5d0819c36add334fcaafbc19b3ce1a3e263a540
malekkonrad <konrad.malek1@gmail.com>
2025-04-08 22:32:10 +0200

    small changes for JsonReader

--- utils/src/main/java/pl/project/json/JsonReader.java
+++ utils/src/main/java/pl/project/json/JsonReader.java
@@ -11,22 +11,12 @@ public class JsonReader {
 
     CommandList commandList;
 
+
+    // TODO zrobić go uniwersalnym, żeby pozbyć się @LaneManager
     public JsonReader(String filePath) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
 
-        // Wczytanie JSON-a z pliku
         this.commandList = mapper.readValue(new File(filePath), CommandList.class);
-
-        // Przejście po komendach
-//        for (Command cmd : commandList.getCommands()) {
-//            System.out.println("Type: " + cmd.getType());
-//            if ("addVehicle".equals(cmd.getType())) {
-//                System.out.println("  ID: " + cmd.getVehicleId());
-//                System.out.println("  Start: " + cmd.getStartRoad());
-//                System.out.println("  End: " + cmd.getEndRoad());
-//            }
-//        }
-
     }
 
     public CommandList read(){
