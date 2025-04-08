e35be963f2d215cfd2a32082eee2f3d32082339a
malekkonrad <konrad.malek1@gmail.com>
2025-04-08 18:49:23 +0200

    updated StepStatus, StepStatusList

--- utils/src/main/java/pl/project/json/structures/output/StepStatus.java
+++ utils/src/main/java/pl/project/json/structures/output/StepStatus.java
@@ -1,13 +1,21 @@
 package pl.project.json.structures.output;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StepStatus {
 
     private List<String> leftVehicles;
+
+    public boolean equals(StepStatus stepStatus) {
+        return leftVehicles.equals(stepStatus.leftVehicles);
+    }
 }
--- utils/src/main/java/pl/project/json/structures/output/StepStatusList.java
+++ utils/src/main/java/pl/project/json/structures/output/StepStatusList.java
@@ -1,12 +1,28 @@
 package pl.project.json.structures.output;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StepStatusList {
-    private List<StepStatus> stepStatuses;
+    private List<StepStatus> stepStatuses = new ArrayList<StepStatus>();
+
+    public void addStep(List<String> stepsStatusString ) {
+
+        StepStatus stepStatus = new StepStatus(stepsStatusString);
+        this.stepStatuses.add(stepStatus);
+    }
+
+    public boolean equals(StepStatusList stepStatusList) {
+        return this.stepStatuses.equals(stepStatusList.stepStatuses);
+    }
+
 }
