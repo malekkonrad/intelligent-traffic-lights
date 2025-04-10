222e0ffcd3a9ce28c2ad522f5f453e202412720c
malekkonrad <konrad.malek1@gmail.com>
2025-04-10 10:23:52 +0200

    added correct reading file and saving options

--- main/src/main/java/pl/project/App.java
+++ main/src/main/java/pl/project/App.java
@@ -21,7 +21,25 @@ import pl.project.json.structures.output.StepStatusList;
  */
 public class App 
 {
-    public static void main( String[] args ) throws Exception {
+    static String inputFile = "commands.json";
+    static String outputFile = "output.json";
+
+
+    public static void main( String... args ) throws Exception {
+
+        // parsing files name
+        try{
+
+            inputFile = args[0];
+            outputFile = args[1];
+
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
 
         // wczytanie ustawień skrzyżowania!
         LaneManager laneManager = new LaneManager();
@@ -36,7 +54,7 @@ public class App
         }
 
         // wczytanie listy komend
-        JsonReader jsonReader = new JsonReader("commands.json");
+        JsonReader jsonReader = new JsonReader(inputFile);
         CommandList commandList = jsonReader.read();
 
         // Obiekt w którym będę zapisywał statusy
@@ -47,8 +65,9 @@ public class App
 
         // Przejście po komendach
         for (Command cmd : commandList.getCommands()) {
-            System.out.println("Type: " + cmd.getType());
+            System.out.print("Type: " + cmd.getType());
             if ("addVehicle".equals(cmd.getType())) {
+                System.out.print(" directions: " + cmd.getStartRoad() + " " + cmd.getEndRoad() + "\n");
                 intersection.addVehicle(new Vehicle(cmd.getVehicleId(),0, cmd.getStartRoad(), cmd.getEndRoad()));
             }else{
                 List<String> leftVehicle = intersection.step();
@@ -60,7 +79,7 @@ public class App
 
 
         // zapis
-        JsonWriter jsonWriter = new JsonWriter("output.json");
+        JsonWriter jsonWriter = new JsonWriter(outputFile);
         jsonWriter.writeToFile(stepStatusList);
 
 
