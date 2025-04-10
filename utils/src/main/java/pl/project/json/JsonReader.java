5961c331c68d434e881463a14e3e4a0f862ad355
malekkonrad <konrad.malek1@gmail.com>
2025-04-10 23:00:27 +0200

    updated version of JsonReader

--- utils/src/main/java/pl/project/json/JsonReader.java
+++ utils/src/main/java/pl/project/json/JsonReader.java
@@ -1,28 +1,47 @@
 package pl.project.json;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.project.json.structures.Direction;
+import pl.project.json.structures.Lane;
 import pl.project.json.structures.input.CommandList;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 
 public class JsonReader {
 
-    CommandList commandList;
-
-
-    // TODO zrobić go uniwersalnym, żeby pozbyć się @LaneManager
-    public JsonReader(String filePath) throws IOException {
+    public static CommandList loadCommandList(String filePath) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-
-        this.commandList = mapper.readValue(new File(filePath), CommandList.class);
-    }
-
-    public CommandList read(){
-        return commandList;
+        return mapper.readValue(new File(filePath), CommandList.class);
     }
 
 
+    public static Map<Direction, List<Lane>> loadLanesFromJson(String filePath) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(new File(filePath));
+        Map<Direction, List<Lane>> lanesPerDirection = new EnumMap<>(Direction.class);
+
+        for (Direction dir : Direction.values()) {
+            JsonNode laneArray = root.get(dir.name().toLowerCase());
+            if (laneArray == null || !laneArray.isArray()) continue;
+
+            List<Lane> laneList = new ArrayList<>();
+            for (JsonNode laneNode : laneArray) {
+                List<Direction> allowedDestinations = new ArrayList<>();
+                for (JsonNode exit : laneNode.get("allowedDestinations")) {
+                    allowedDestinations.add(Direction.valueOf(exit.asText().toUpperCase()));
+                }
+                laneList.add(new Lane(allowedDestinations));
+            }
+            lanesPerDirection.put(dir, laneList);
+        }
+        return lanesPerDirection;
+    }
 
 
 }
